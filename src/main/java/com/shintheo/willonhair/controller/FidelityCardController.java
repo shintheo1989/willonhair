78993ae3efd35d5a51a0de8a5327a4eb22aa913d
Leutou Patrice <patriceleutou@gmail.com>
2023-01-29 16:12:40 +0200

    Minor update on fidelityController

--- src/main/java/com/shintheo/willonhair/controller/FidelityCardController.java
+++ src/main/java/com/shintheo/willonhair/controller/FidelityCardController.java
@@ -2,19 +2,16 @@ package com.shintheo.willonhair.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityNotFoundException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shintheo.willonhair.api.FidelityCardApi;
-import com.shintheo.willonhair.entity.EmployeeDao;
 import com.shintheo.willonhair.entity.UserDao;
 import com.shintheo.willonhair.entity.fidelity.BonusIncrementDao;
 import com.shintheo.willonhair.entity.fidelity.CardDao;
-import com.shintheo.willonhair.service.EmployeeService;
+import com.shintheo.willonhair.service.UserService;
 import com.shintheo.willonhair.service.fidelity.CardService;
 
 import lombok.NoArgsConstructor;
@@ -26,20 +23,20 @@ public class FidelityCardController implements FidelityCardApi {
 	@Autowired
     CardService fidelityCardSrv;
 	
-	@Autowired EmployeeService employeeService;
+	@Autowired UserService userService;
 	
 	@Override
 	public CardDao incrementFidelity(@PathVariable(name = "id") Long userId) {
-		EmployeeDao employee = employeeService.findEmployeeById(userId).orElseThrow(() -> new EntityNotFoundException());
+		UserDao user = userService.getUserById(userId);
 		// Init card in case it is not done yet
-		fidelityCardSrv.initUserCard(employee.getUser());
+		fidelityCardSrv.initUserCard(user);
 		// Increment points
-		return fidelityCardSrv.incrementUserFidelityPoint(employee.getUser());		
+		return fidelityCardSrv.incrementUserFidelityPoint(user);		
 	}
 	
 	@Override
 	public List<BonusIncrementDao> listUserBonuses(@PathVariable(name = "id") Long userId) {
-		UserDao user = employeeService.findUserById(userId).orElseThrow(() -> new EntityNotFoundException());
+		UserDao user = userService.getUserById(userId);
 		CardDao card = fidelityCardSrv.getUserCard(user);
 		return card.getBonuses();
 	}
