5a776c3bd8fded936bf96f19127d4833b57dd3a6
Leutou Patrice <patriceleutou@gmail.com>
2023-01-30 19:21:42 +0200

    Increase lifetime of JWT Tokens

--- src/main/java/com/shintheo/willonhair/serviceImpl/JwtServiceImpl.java
+++ src/main/java/com/shintheo/willonhair/serviceImpl/JwtServiceImpl.java
@@ -45,7 +45,7 @@ public class JwtServiceImpl implements JwtService {
 				.setClaims(extraClaims)
 				.setSubject(userDetails.getUsername())
 				.setIssuedAt(new Date(System.currentTimeMillis()))
-				.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+				.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24 * 24))
 				.signWith(getSigninKey(), SignatureAlgorithm.HS256)
 				.compact();
  
