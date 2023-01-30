6122d9455056d2fc46845bf33aabf30caf0f7ae1
Leutou Patrice <patriceleutou@gmail.com>
2023-01-30 07:06:28 +0200

    Minor adds

--- src/main/java/com/shintheo/willonhair/repository/EmployeeRepository.java
+++ src/main/java/com/shintheo/willonhair/repository/EmployeeRepository.java
@@ -1,9 +1,14 @@
 package com.shintheo.willonhair.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-import com.shintheo.willonhair.entity.EmployeeDao;
+import com.shintheo.willonhair.entity.UserDao;
+import com.shintheo.willonhair.base.Type;
 
-public interface EmployeeRepository extends JpaRepository<EmployeeDao, Long> {
-
+public interface EmployeeRepository extends JpaRepository<UserDao, Long> {
+	@Query(value = "SELECT u FROM UserDao u WHERE u.type =:type" )
+	public List<UserDao> findAll(Type type);
 }
--- src/main/java/com/shintheo/willonhair/repository/UserRepository.java
+++ src/main/java/com/shintheo/willonhair/repository/UserRepository.java
@@ -3,11 +3,9 @@ package com.shintheo.willonhair.repository;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import com.shintheo.willonhair.entity.UserDao;
 
-@Repository
 public interface UserRepository extends CrudRepository<UserDao, Long> {
 	Optional<UserDao> findByEmail(String email);
 }
